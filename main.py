b68d37599d1239d8ecfa482a895886128e6b8ab3
Jayson Ryter <jjryter1@gmail.com>
2021-04-10 22:13:57 -0400

    adding on member join

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 import os
 import random
 from dotenv import load_dotenv
-
-# 1
 from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# 2
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_member_join(member):
+  await member.create_dm()
+  await member.dm_channel(
+      f'Hey {member.name}, you\'re finally awake. Welcome to the Auh! Discord server!'
+  )
 
 bot.run(TOKEN)
