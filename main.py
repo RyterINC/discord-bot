5413fa59791b8a8f7dd5b4bba5598643804bcaa9
Jayson Ryter <jjryter1@gmail.com>
2021-04-11 21:15:44 -0400

    adding boto3
    - adding boto3 package, creating state json file in s3
    - including logic to upload / download file based on existence

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# bot.py
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-# import cogs
+import boto3
+from botocore.exceptions import ClientError
 from cogs import greetings, util
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+BUCKET_NAME = os.getenv('DISCORD_S3_BUCKET')
+GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
@@ -17,6 +19,29 @@ bot.add_cog(util.Utility(bot))
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+
+s3 = boto3.resource('s3')
+filename = GUILD + "_" + "state.json"
+bucket = s3.Bucket(BUCKET_NAME)
+objs = list(bucket.objects.filter(Prefix=filename))
+path = "./state/"
+
+if os.path.isdir(path) == False:
+    os.mkdir("./state")
+
+if any([w.key == filename for w in objs]):
+    print("State file exists for " + GUILD + " Discord server...")
+    if os.path.isfile(path + filename):
+        os.remove(path + filename)
+
+    s3.meta.client.download_file(BUCKET_NAME, filename, path + filename)
+else:
+    print("State file does not exist for " + GUILD + " Discord server, creating...")
+    f = open(path + filename, 'w')
+    f.write('{}')
+    s3.meta.client.upload_file(path + filename, BUCKET_NAME, filename)
+
 
 
 bot.run(TOKEN)
