181f71d1b4560ef2a6f754a85f46445503036afe
Jayson Ryter <jjryter1@gmail.com>
2021-04-14 18:26:07 -0400

    creating state file initially from template file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-from dotenv import load_dotenv
-from discord.ext import commands
-import boto3
 from botocore.exceptions import ClientError
 from cogs import greetings, util
+from discord.ext import commands
+from dotenv import load_dotenv
+from shutil import copyfile
+import boto3
+import json
+import os
 
 
 load_dotenv()
@@ -20,28 +22,28 @@ bot.add_cog(util.Utility(bot))
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-
-s3 = boto3.resource('s3')
+path = "./state/"
 filename = GUILD + "_" + "state.json"
+stateFilePath = path + filename
+templateFilePath = "./templates/state-template.json"
+s3 = boto3.resource('s3')
 bucket = s3.Bucket(BUCKET_NAME)
 objs = list(bucket.objects.filter(Prefix=filename))
-path = "./state/"
 
 if os.path.isdir(path) == False:
     os.mkdir("./state")
 
 if any([w.key == filename for w in objs]):
     print("State file exists for " + GUILD + " Discord server...")
-    if os.path.isfile(path + filename):
-        os.remove(path + filename)
+    if os.path.isfile(stateFilePath):
+        os.remove(stateFilePath)
 
-    s3.meta.client.download_file(BUCKET_NAME, filename, path + filename)
+    s3.meta.client.download_file(BUCKET_NAME, filename, stateFilePath)
 else:
     print("State file does not exist for " + GUILD + " Discord server, creating...")
-    f = open(path + filename, 'w')
-    f.write('{}')
-    s3.meta.client.upload_file(path + filename, BUCKET_NAME, filename)
 
+    copyfile(templateFilePath, stateFilePath)
+    s3.meta.client.upload_file(stateFilePath, BUCKET_NAME, filename)
 
 
 bot.run(TOKEN)
