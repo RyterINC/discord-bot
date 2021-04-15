cd372d3016638be544b2d25cef8d91a4b7fb617c
Jayson Ryter <jjryter1@gmail.com>
2021-04-15 15:27:27 -0400

    adding group_delete and group_list

--- cogs/groups.py
+++ cogs/groups.py
@@ -14,10 +14,10 @@ class Groups(commands.Cog):
 
 
     @commands.command(name='group_create', help='Creates a notification group')
-    async def roll(self, ctx, group_name):
+    async def group_create(self, ctx, group_name):
         with open(self.stateFilePath) as infile:
             data = json.load(infile)
-        
+
         if group_name in data["groups"]:
             message = "Group \"" + group_name + "\" already exists!"
         else:
@@ -26,4 +26,33 @@ class Groups(commands.Cog):
                 json.dump(data, outfile, sort_keys=True, indent=4)
             message = "Group \"" + group_name + "\" created!"
             self.s3.meta.client.upload_file(self.stateFilePath, self.BUCKET_NAME, self.filename)
+
+        await ctx.send(message)
+
+
+    @commands.command(name='group_delete', help='Deletes a notification group')
+    async def group_delete(self, ctx, group_name):
+        with open(self.stateFilePath) as infile:
+            data = json.load(infile)
+
+        if group_name not in data["groups"]:
+            message = "Group \"" + group_name + "\" doesn't exist!"
+        else:
+            del data["groups"][group_name]
+            with open(self.stateFilePath, 'w') as outfile:
+                json.dump(data, outfile, sort_keys=True, indent=4)
+            message = "Group \"" + group_name + "\" deleted!"
+            self.s3.meta.client.upload_file(self.stateFilePath, self.BUCKET_NAME, self.filename)
+
+        await ctx.send(message)
+
+
+    @commands.command(name='group_list', help='List all notification groups')
+    async def group_list(self, ctx):
+        message = '❗Current notification groups❗\n>>> '
+        with open(self.stateFilePath) as infile:
+            data = json.load(infile)
+        for group in data["groups"].keys():
+            message = message + group + '\n'
+
         await ctx.send(message)
